878af2fba19876fb7972c404c29db07e98bb39b6
eburbanol <eburbanol@unal.edu.co>
2018-10-02 13:54:13 -0500

    changes

--- myfloat_func.py
+++ myfloat_func.py
@@ -1,37 +1,52 @@
 # -*- coding: utf-8 -*-
 
-a=(['+',3,2,1,4,1],[2,4,6,2,6,2,4])
+a=([3,2,1,4,1,"+"],[2,4,6,2,6,2,4])
 a=str(a)
 
 
 def imprimir(a):
-    c=''
-    for x in a[0] : 
-        c = c+''.join(x) 
-    print (c)
-
-    
-    
-
+    for i in range(len(a[0]),0,-1):
+        print (a[0][i-1], end='',)
+    print (',', end='')
+    for i in range (len(a[1],0,-1):
+        print (a[1][i-1],end='',)
+                                
+        
 def suma(a, b):
-    resul=(d,e)
-    for i in range(len(a[0])):
-      resul.append(a[i])
-
-    for i in range(len(b[0])):
-      d[i] = a[i] +b[i]
-    
-    for i in range(len(a[1])):
-      resul.append(a[i])
-
-    for i in range(len(b[1])):
-      e[i] = a[i] +b[i]
-    resul=(d,e)
-
-    print(resul)
-    
-
-
+    a[0].reverse()
+    b[0].reverse()
+    a_deci=''.join(str(e) for e in a[1]
+    b_deci=''.join(str(e) for e in a[1]
+    a_int=''.join(str(e) for e in a[1]
+    b_int=''.join(str(e) for e in a[1]
+        if a[0][1]=="-":              
+            a_deci=float(a_int.replace("-",""))
+            a_int=float("0."+a_int)
+            a_deci=a_deci*-1
+            a_int=a_int*-1
+        else:
+            a_deci=float(a_deci.replace("+",""))
+            a_int=float("0."+a_int)
+        if b[0][1]=="-":              
+            b_deci=float(a_int.replace("-",""))
+            b_int=float("0."+b_int)
+            b_deci=b_deci*-1
+            b_int=b_int*-1
+        else:
+            b_deci=float(b_deci.replace("+",""))
+            b_int=float("0."+b_int)
+        a[0].reverse()               
+        b[0].reverse()
+                  
+        a_new=a_int+a_deci
+        b_new=b_int+b_deci
+        c_resul=a_new+b_new
+                  
+        if c_resul>0:
+            
+                  
+                  
+                  
 def resta(a, b):
     pass
 
