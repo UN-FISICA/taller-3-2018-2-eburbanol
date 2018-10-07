4527d85d8dbfcfe0c7027c700740debc8bb87270
eburbanol <eburbanol@unal.edu.co>
2018-10-07 16:50:53 -0500

    Update myfloat_func.py

--- myfloat_func.py
+++ myfloat_func.py
@@ -1,263 +1,456 @@
 # -*- coding: utf-8 -*-
-
-a=([3,2,1,4,1,"+"],[2,4,6,2,6,2,4])
-a=str(a)
-
-
-def imprimir(a):
+def imprimir (a):
     a0=a[0]
     a1=a[1]
-
+    
     #print (type(a0))
-
+    
     a0.reverse()
-    a0.append(',')
-
+    a0.append('.')
+    
     #b0=str(a0)
     #print (type(b0[3]))
-
+    
     #print (a0)
     resul=a0+a1
-
-    #print (resul)
-
-    print (''.join(map(str,resul)))  
-                                
-        
-def suma(a, b):
-    a[0].reverse()
-    b[0].reverse()
-    a_deci=''.join(str(e) for e in a[1]
-    b_deci=''.join(str(e) for e in a[1]
-    a_int=''.join(str(e) for e in a[1]
-    b_int=''.join(str(e) for e in a[1]
-        if a[0][1]=="-":              
-            a_deci=float(a_int.replace("-",""))
-            a_int=float("0."+a_int)
-            a_deci=a_deci*-1
-            a_int=a_int*-1
-        else:
-            a_deci=float(a_deci.replace("+",""))
-            a_int=float("0."+a_int)
-        if b[0][1]=="-":              
-            b_deci=float(a_int.replace("-",""))
-            b_int=float("0."+b_int)
-            b_deci=b_deci*-1
-            b_int=b_int*-1
-        else:
-            b_deci=float(b_deci.replace("+",""))
-            b_int=float("0."+b_int)
-        a[0].reverse()               
-        b[0].reverse()
-                  
-        a_new=a_int+a_deci
-        b_new=b_int+b_deci
-        c_resul=a_new+b_new
-                  
-        if c_resul>0:
-            resul_int=int(c_resul)
-            resul_deci=c_resul-int(c_resul)
-            E="+"+str(resul_int)
-            D=list(resul_deci)
-            E=list(e)
-            D=list(D)
-        else:
-        resul_int=int(c_resul)
-        resul_deci=c_resul+int(c_resul)           
-        E=str(resul_int)     
-        D=str(resul_deci)
-        E=list(E)
-        D=list(D)
-    c=(E,D)
-    return c
-                  
-                  
-def resta(a, b):
-    a[0].reverse()
-    b[0].reverse()
-    a_deci=''.join(str(e) for e in a[1]
-    b_deci=''.join(str(e) for e in a[1]
-    a_int=''.join(str(e) for e in a[1]
-    b_int=''.join(str(e) for e in a[1]
-        if a[0][1]=="-":              
-            a_deci=float(a_int.replace("-",""))
-            a_int=float("0."+a_int)
-            a_deci=a_deci*-1
-            a_int=a_int*-1
-        else:
-            a_deci=float(a_deci.replace("+",""))
-            a_int=float("0."+a_int)
-        if b[0][1]=="-":              
-            b_deci=float(a_int.replace("-",""))
-            b_int=float("0."+b_int)
-            b_deci=b_deci*-1
-            b_int=b_int*-1
-        else:
-            b_deci=float(b_deci.replace("+",""))
-            b_int=float("0."+b_int)
-        a[0].reverse()               
-        b[0].reverse()
-                  
-        a_new=a_int+a_deci
-        b_new=b_int+b_deci
-        c_resul=a_new-b_new
-                  
-        if c_resul>0:
-            resul_int=int(c_resul)
-            resul_deci=c_resul-int(c_resul)
-            E="+"+str(resul_int)
-            D=list(resul_deci)
-            E=list(e)
-            D=list(D)
-        else:
-        resul_int=int(c_resul)
-        resul_deci=c_resul+int(c_resul)           
-        E=str(resul_int)     
-        D=str(resul_deci)
-        E=list(E)
-        D=list(D)
-    c=(E,D)
-    return c
-
-
-def multiplicacion(a, b):
-    a[0].reverse()
-    b[0].reverse()
-    a_deci=''.join(str(e) for e in a[1]
-    b_deci=''.join(str(e) for e in a[1]
-    a_int=''.join(str(e) for e in a[1]
-    b_int=''.join(str(e) for e in a[1]
-        if a[0][1]=="-":              
-            a_deci=float(a_int.replace("-",""))
-            a_int=float("0."+a_int)
-            a_deci=a_deci*-1
-            a_int=a_int*-1
-        else:
-            a_deci=float(a_deci.replace("+",""))
-            a_int=float("0."+a_int)
-        if b[0][1]=="-":              
-            b_deci=float(a_int.replace("-",""))
-            b_int=float("0."+b_int)
-            b_deci=b_deci*-1
-            b_int=b_int*-1
-        else:
-            b_deci=float(b_deci.replace("+",""))
-            b_int=float("0."+b_int)
-        a[0].reverse()               
-        b[0].reverse()
-                  
-        a_new=a_int+a_deci
-        b_new=b_int+b_deci
-        c_resul=a_new*b_new
-                  
-        if c_resul>0:
-            resul_int=int(c_resul)
-            resul_deci=c_resul-int(c_resul)
-            E="+"+str(resul_int)
-            D=list(resul_deci)
-            E=list(e)
-            D=list(D)
-        else:
-        resul_int=int(c_resul)
-        resul_deci=c_resul+int(c_resul)           
-        E=str(resul_int)     
-        D=str(resul_deci)
-        E=list(E)
-        D=list(D)
-    c=(E,D)
-    return c
-
-
-def division(a, b):
-    a[0].reverse()
-    b[0].reverse()
-    a_deci=''.join(str(e) for e in a[1]
-    b_deci=''.join(str(e) for e in a[1]
-    a_int=''.join(str(e) for e in a[1]
-    b_int=''.join(str(e) for e in a[1]
-        if a[0][1]=="-":              
-            a_deci=float(a_int.replace("-",""))
-            a_int=float("0."+a_int)
-            a_deci=a_deci*-1
-            a_int=a_int*-1
-        else:
-            a_deci=float(a_deci.replace("+",""))
-            a_int=float("0."+a_int)
-        if b[0][1]=="-":              
-            b_deci=float(a_int.replace("-",""))
-            b_int=float("0."+b_int)
-            b_deci=b_deci*-1
-            b_int=b_int*-1
-        else:
-            b_deci=float(b_deci.replace("+",""))
-            b_int=float("0."+b_int)
-        a[0].reverse()               
-        b[0].reverse()
-                  
-        a_new=a_int+a_deci
-        b_new=b_int+b_deci
-        c_resul=a_new/b_new
-                  
-        if c_resul>0:
-            resul_int=int(c_resul)
-            resul_deci=c_resul-int(c_resul)
-            E="+"+str(resul_int)
-            D=list(resul_deci)
-            E=list(e)
-            D=list(D)
-        else:
-        resul_int=int(c_resul)
-        resul_deci=c_resul+int(c_resul)           
-        E=str(resul_int)     
-        D=str(resul_deci)
-        E=list(E)
-        D=list(D)
-    c=(E,D)
-    return c
-
-
-def comparacion(a, b):
-    a[0].reverse()
-    b[0].reverse()
-    a_deci=''.join(str(e) for e in a[1]
-    b_deci=''.join(str(e) for e in a[1]
-    a_int=''.join(str(e) for e in a[1]
-    b_int=''.join(str(e) for e in a[1]
-        if a[0][1]=="-":              
-            a_deci=float(a_int.replace("-",""))
-            a_int=float("0."+a_int)
-            a_deci=a_deci*-1
-            a_int=a_int*-1
-        else:
-            a_deci=float(a_deci.replace("+",""))
-            a_int=float("0."+a_int)
-        if b[0][1]=="-":              
-            b_deci=float(a_int.replace("-",""))
-            b_int=float("0."+b_int)
-            b_deci=b_deci*-1
-            b_int=b_int*-1
-        else:
-            b_deci=float(b_deci.replace("+",""))
-            b_int=float("0."+b_int)
-        a[0].reverse()               
-        b[0].reverse()
-                  
-        a_new=a_int+a_deci
-        b_new=b_int+b_deci
-        if a_new==b_new
-            return ("true")
-        else
-            return ("false")
     
+    #print (resul)
+    
+    print (''.join(map(str,resul)))
+    
+def suma(a,b):
+    """
+        
+    Para la tupla a
+        
+        
+    """
+    a0=a[0]
+    a1=a[1]
+    a0.reverse()
+    a0.append('.')
+    a=a0+a1
+        
+        
+    if a[0]=='+':
+        a.remove('+')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float
+        
+    else:
+        a.remove('-')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float*-1.
+    #print (type(a))
+    #print (a)
+        
+    """
+        
+    Para la tupla b
+        
+        
+    """
+        
+    b0=b[0]
+    b1=b[1]    
+    b0.reverse()
+    b0.append('.')
+    b=b0+b1
+    if b[0]=='+':
+        b.remove('+')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float
+    else:
+        b.remove('-')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float*-1.
+    #print (type(b))
+    #print (b)
+        
+    c=a+b
+    #print (type(c))
+       
+    if c>=0: 
+        c0=int(c)
+        c1=abs(c)-abs(int(c))  
+            
+        c0=str(c0)        
+        c0=list(c0)        
+            
+        c1=str(c1)        
+        c1=list(c1)
+                  
+        c0.insert(0,'+')
+        c0.reverse()
+           
+                   
+        c1.remove('0')
+        c1.remove('.')
+        c=(c0,c1)
+            
+    else:
+        c0=int(c)
+        c1=abs(c)-abs(int(c))  
+            
+        c0=str(c0)        
+        c0=list(c0)        
+            
+        c1=str(c1)        
+        c1=list(c1)
+                  
+        c0.insert(0,'-')
+        c0.reverse()
+           
+                   
+        c1.remove('0')
+        c1.remove('.')
+        c=(c0,c1)
+            
+    print (c)
+        
+       
+        
+def resta(a,b):
+    """
+    
+    Para la tupla a
+    
+    
+    """
+    a0=a[0]
+    a1=a[1]
+    a0.reverse()
+    a0.append('.')
+    a=a0+a1
+    
+    
+    if a[0]=='+':
+        a.remove('+')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float
+    
+    else:
+        a.remove('-')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float*-1.
+    #print (type(a))
+    #print (a)
+    
+    """
+    
+    Para la tupla b
+    
+    
+    """
+    
+    b0=b[0]
+    b1=b[1]    
+    b0.reverse()
+    b0.append('.')
+    b=b0+b1
+    if b[0]=='+':
+        b.remove('+')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float
+    else:
+        b.remove('-')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float*-1.
+    #print (type(b))
+    #print (b)
+    
+    c=a-b
+    #print (type(c))
+    if c>=0: 
+        c0=int(c)
+        c1=abs(c)-abs(int(c))  
+            
+        c0=str(c0)        
+        c0=list(c0)        
+            
+        c1=str(c1)        
+        c1=list(c1)
+                  
+        c0.insert(0,'+')
+        c0.reverse()
+           
+                   
+        c1.remove('0')
+        c1.remove('.')
+        c=(c0,c1)
+            
+          
+    else:
+        c0=int(c)
+        c1=abs(c)-abs(int(c))  
+            
+        c0=str(c0)        
+        c0=list(c0)        
+            
+        c1=str(c1)        
+        c1=list(c1)
+                  
+            
+        c0.insert(0,'-')
+        c0.reverse()
+           
+                   
+        c1.remove('0')
+        c1.remove('.')
+        c=(c0,c1)
+            
+        print (c)
+def multiplicacion(a,b):
+"""
+    
+    Para la tupla a
+    
+    
+    """
+    a0=a[0]
+    a1=a[1]
+    a0.reverse()
+    a0.append('.')
+    a=a0+a1
+    
+    
+    if a[0]=='+':
+        a.remove('+')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float
+    
+    else:
+        a.remove('-')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float*-1.
+    #print (type(a))
+    #print (a)
+    
+    """
+    
+    Para la tupla b
+    
+    
+    """
+    
+    b0=b[0]
+    b1=b[1]    
+    b0.reverse()
+    b0.append('.')
+    b=b0+b1
+    if b[0]=='+':
+        b.remove('+')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float
+    else:
+        b.remove('-')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float*-1.
+    #print (type(b))
+    #print (b)
+    
+    c=a*b
+    #print (type(c))
+    if c>=0: 
+        c0=int(c)
+        c1=abs(c)-abs(int(c))  
+            
+        c0=str(c0)        
+        c0=list(c0)        
+            
+        c1=str(c1)        
+        c1=list(c1)
+                  
+        c0.insert(0,'+')
+        c0.reverse()
+           
+                   
+        c1.remove('0')
+        c1.remove('.')
+        c=(c0,c1)
+            
+          
+    else:
+        c0=int(c)
+        c1=abs(c)-abs(int(c))  
+            
+        c0=str(c0)        
+        c0=list(c0)        
+            
+        c1=str(c1)        
+        c1=list(c1)
+                  
+            
+        c0.insert(0,'-')
+        c0.reverse()
+           
+                   
+        c1.remove('0')
+        c1.remove('.')
+        c=(c0,c1)    
 
+def division(a,b):   
+"""
+    
+    Para la tupla a
+    
+    
+    """
+    a0=a[0]
+    a1=a[1]
+    a0.reverse()
+    a0.append('.')
+    a=a0+a1
+    
+    
+    if a[0]=='+':
+        a.remove('+')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float
+    
+    else:
+        a.remove('-')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float*-1.
+    #print (type(a))
+    #print (a)
+    
+    """
+    
+    Para la tupla b
+    
+    
+    """
+    
+    b0=b[0]
+    b1=b[1]    
+    b0.reverse()
+    b0.append('.')
+    b=b0+b1
+    if b[0]=='+':
+        b.remove('+')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float
+    else:
+        b.remove('-')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float*-1.
+    #print (type(b))
+    #print (b)
+    
+    c=a/b
+    #print (type(c))
+    if c>=0: 
+        c0=int(c)
+        c1=abs(c)-abs(int(c))  
+            
+        c0=str(c0)        
+        c0=list(c0)        
+            
+        c1=str(c1)        
+        c1=list(c1)
+                  
+        c0.insert(0,'+')
+        c0.reverse()
+           
+                   
+        c1.remove('0')
+        c1.remove('.')
+        c=(c0,c1)
+            
+          
+    else:
+        c0=int(c)
+        c1=abs(c)-abs(int(c))  
+            
+        c0=str(c0)        
+        c0=list(c0)        
+            
+        c1=str(c1)        
+        c1=list(c1)
+                  
+            
+        c0.insert(0,'-')
+        c0.reverse()
+           
+                   
+        c1.remove('0')
+        c1.remove('.')
+        c=(c0,c1)
+
+def comparacion(a,b):
+"""
+    
+    Para la tupla a
+    
+    
+    """
+    a0=a[0]
+    a1=a[1]
+    a0.reverse()
+    a0.append('.')
+    a=a0+a1
+    
+    
+    if a[0]=='+':
+        a.remove('+')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float
+    
+    else:
+        a.remove('-')        
+        a_str= ''.join(str(e)for e in a)
+        a_float=float(a_str)
+        a=a_float*-1.
+    #print (type(a))
+    #print (a)
+    
+    """
+    
+    Para la tupla b
+    
+    
+    """
+    
+    b0=b[0]
+    b1=b[1]    
+    b0.reverse()
+    b0.append('.')
+    b=b0+b1
+    if b[0]=='+':
+        b.remove('+')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float
+    else:
+        b.remove('-')        
+        b_str= ''.join(str(e)for e in b)
+        b_float=float(b_str)
+        b=b_float*-1.
+    if a==b:
+        return ("true")
+    else:
+        return ("False")
 
 def pi():
     pass
+if __name__=="__main__":
+    print (imprimir(pi()))
 
-
-if __name__ == "__main__":
-    print(imprimir(pi()))
-
-
-print (a)
-imprimir (a)
